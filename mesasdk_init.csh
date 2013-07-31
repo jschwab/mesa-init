23e514aa2155ea0c6c787e76ea5d2b21e5ac3bfc
Josiah Schwab <jschwab@gmail.com>
2013-07-31 11:43:56 -0700

    revise init scripts to behave like python virtualenvs

--- mesasdk_init.csh
+++ mesasdk_init.csh
@@ -1,5 +1,8 @@
 # $Id: mesasdk_init.csh 187 2012-08-09 07:33:23Z townsend $
 
+# The enviroment managment portions of these scripts were inspired by
+# the scripts shipped with the python virtualenv package
+
 # Check that MESASDK_ROOT is set
 
 if ( ! ${?MESASDK_ROOT} ) then
@@ -12,13 +15,33 @@ if ( `${MESASDK_ROOT}/bin/mesasdk_arch_check.pl` != 'Y' ) then
     echo "mesasdk_init.csh: unsupported architecture"
 endif
 
-# Set paths
+# Define an alias to deactivate the SDK
+alias deactivatesdk 'test $?_OLD_MESASDK_PATH != 0 && setenv PATH "$_OLD_MESASDK_PATH" && unset _OLD_MESASDK_PATH; test $?_OLD_MESASDK_MANPATH != 0 && setenv MANPATH "$_OLD_MESASDK_MANPATH" && unset _OLD_MESASDK_MANPATH; test $?_OLD_MESASDK_MANPATH = 0 && test "\!:*" != "nondestructive" && unset MANPATH ; rehash; test $?_OLD_MESASDK_PGPLOT_DIR != 0 && set PGPLOT_DIR="$_OLD_MESASDK_PGPLOT_DIR" && unset _OLD_MESASDK_PGPLOT_DIR; test $?_OLD_MESASDK_PROMPT != 0 && set prompt="$_OLD_MESASDK_PROMPT" && unset _OLD_MESASDK_PROMPT; test "\!:*" != "nondestructive" && unalias deactivatesdk'
 
+# make the script idempotent by calling deactivate nondestructively
+deactivatesdk nondestructive
+
+# Save old paths & set new paths
+
+set _OLD_MESASDK_PATH="$PATH"
 setenv PATH "${MESASDK_ROOT}/bin:${PATH}"
+
 if ( ${?MANPATH} ) then
+    set _OLD_MESASDK_MANPATH="$MANPATH"
     setenv MANPATH "${MESASDK_ROOT}/share/man:${MANPATH}"
 else
-    setenv MANPATH "${MESASDK_ROOT}/share/man"
+    setenv MANPATH "${MESASDK_ROOT}/share/man:"
 endif
 
+# force new paths to take effect
+rehash
+
+# take care of PGPLOT
+set _OLD_MESASDK_PGPLOT_DIR="$PATH"
 setenv PGPLOT_DIR "${MESASDK_ROOT}/pgplot"
+
+# Alter the command prompt unless instructed not to
+if ( ! ${?MESASDK_DISABLE_PROMPT} ) then
+    set _OLD_MESASDK_PROMPT="$prompt"
+    set prompt = "[mesasdk] $prompt"
+endif
--- mesasdk_init.sh
+++ mesasdk_init.sh
@@ -1,5 +1,8 @@
 # $Id: mesasdk_init.sh 187 2012-08-09 07:33:23Z townsend $
 
+# The enviroment managment portions of these scripts were inspired by
+# the scripts shipped with the python virtualenv package
+
 # Check that MESASDK_ROOT is set
 
 if [[ -z "$MESASDK_ROOT" ]]; then
@@ -12,9 +15,75 @@ if [ `${MESASDK_ROOT}/bin/mesasdk_arch_check.pl` != 'Y' ]; then
     echo "mesasdk_init.sh: unsupported architecture"
 fi
 
-# Set paths
+# Define a function to deactivate the SDK
 
+deactivatesdk () {
+
+    # reset old environment variables
+
+    # reset the paths
+    if [ -n "$_OLD_MESASDK_PATH" ] ; then
+        PATH="$_OLD_MESASDK_PATH"
+        export PATH
+        unset _OLD_MESASDK_PATH
+    fi
+
+    if [ -n "$_OLD_MESASDK_MANPATH" ] ; then
+        MANPATH="$_OLD_MESASDK_MANPATH"
+        export MANPATH
+        unset _OLD_MESASDK_MANPATH
+    else
+        if [ ! "$1" = "nondestructive" ] ; then
+            unset MANPATH
+        fi
+    fi
+
+    # force new paths to take effect
+    hash -r 2>/dev/null
+
+    # take care of PGPLOT
+    if [ -n "$_OLD_MESASDK_PGPLOT_DIR" ] ; then
+        PGPLOT_DIR="$_OLD_MESASDK_PGPLOT_DIR"
+        export PGPLOT_DIR
+        unset _OLD_MESASDK_PGPLOT_DIR
+    fi
+
+    # reset the command prompt
+    if [ -n "$_OLD_MESASDK_PS1" ] ; then
+        export PS1="$_OLD_MESASDK_PS1"
+        unset _OLD_MESASDK_PS1
+    fi
+
+    # Self destruct!
+    if [ ! "$1" = "nondestructive" ] ; then
+        unset -f deactivatesdk
+    fi
+}
+
+# make the script idempotent by calling deactivate nondestructively
+deactivatesdk nondestructive
+
+# Save old paths & set new paths
+
+_OLD_MESASDK_PATH="$PATH"
 export PATH="${MESASDK_ROOT}/bin:${PATH}"
+
+if [ -n ${MANPATH} ]; then
+    _OLD_MESASDK_MANPATH="$MANPATH"
+fi
 export MANPATH="${MESASDK_ROOT}/share/man:${MANPATH}"
 
+# force new paths to take effect
+hash -r 2>/dev/null
+
+# take care of PGPLOT
+_OLD_MESASDK_PGPLOT_DIR="$PGPLOT_DIR"
 export PGPLOT_DIR="${MESASDK_ROOT}/pgplot"
+
+# Alter the command prompt unless instructed not to
+if [ -z "$MESASDK_DISABLE_PROMPT" ] ; then
+    _OLD_MESASDK_PS1="$PS1"
+    export PS1="[mesasdk] $PS1"
+fi
+
+
